e4e4461f38c5445e2ffb1c8374386bee3ca49218
bl42 <b@Bs-MacBook-Pro.lan>
2016-11-09 16:42:56 -0600

    fixed js file

---
+++ index.js
@@ -0,0 +1,11 @@
+(function count(i){
+ var fizzbuzz = (i % 15 === 0) ? "fizzbuzz": 
+                 (i % 5 === 0) ? "fizz" :
+                   (i % 3 === 0) ? "buzz" :
+                     null;
+ if(fizzbuzz)
+   console.log(fizzbuzz);
+
+ if(i <= 100)
+   count(i+1);
+})(0);
